6da47dd86c56816d47392e568b25dad933a05144
xStaples <daltonspencer112@gmail.com>
2021-03-28 17:15:10 -0700

    frontend api adjustments

--- monolith-frontend/src/app/services/api.service.ts
+++ monolith-frontend/src/app/services/api.service.ts
@@ -1,27 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MOVIE_API } from 'src/environments/environment';
 import { Movie } from '../models/movie.model';
-import { HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
-
-  constructor(private http: HttpClient) { }
+export class ApiService{
 
   
-  // getMovies(){
-  //   return this.http.get()
-  // }
+  constructor(private http:HttpClient) { }
+
   public findMovies(movie: Movie): Observable<any>{
-    const headers = new HttpHeaders;
-    headers.set('x-rapidapi-key', MOVIE_API);
     
-    console.log(headers.set('x-rapidapi-key', MOVIE_API));
+  
     
-    return this.http.post<any>(`https://imdb8.p.rapidapi.com/title/find?q=${movie.name}`,{'headers':headers})
+    return this.http.post<any>(`https://imdb8.p.rapidapi.com/title/find?q=${movie.name}`, movie)
   }
 }
--- monolith-frontend/src/environments/environment.ts
+++ monolith-frontend/src/environments/environment.ts
@@ -5,4 +5,4 @@ export const environment = {
 
 export const BACKEND_URL = `http://localhost:8080/movieapp/` //backend 
 
-export const MOVIE_API = "e897a27ea1msh155caf9564eff84p164e88jsned71f0b765f8" //api
+export const MOVIE_API = 'e897a27ea1msh155caf9564eff84p164e88jsned71f0b765f8' //api
